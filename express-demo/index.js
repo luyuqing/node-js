6df29566eee5152e9a17e22c928d927cd3ccfbce
Yuqing <yuqing@boost.ai>
2018-08-11 15:04:11 +0200

    Add vidly

--- express-demo/index.js
+++ express-demo/index.js
@@ -4,67 +4,14 @@ const config = require('config');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const Joi = require('joi');
-<<<<<<< HEAD
-const logger = require('./logger');
-||||||| merged common ancestors
-=======
 const logger = require('./middleware/logger');
->>>>>>> e7939234a2a0433c3a6d89e7674322d7d7a86cbd
 const express = require('express');
 const courses = require('./routes/courses')
 const home = require('./routes/home')
 const app = express();
 
-<<<<<<< HEAD
-app.use(express.json());
-app.use(logger);
-app.use(function(req, res, next) {
-    console.log('Authenticating...');
-    next();
-});
-app.use(express.static('public'));
-
-const courses = [
-    {id: 1, name: 'course1'},
-    {id: 2, name: 'course2'},
-    {id: 3, name: 'course3'},
-];
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
-// app.get('/api/courses', (req, res) => {
-//     res.send([1, 2, 3]);
-// });
-
-// app.get('/api/courses/:year/:month', (req, res) => {
-//     res.send(req.params);
-// });
-||||||| merged common ancestors
-app.use(express.json());
-
-const courses = [
-    {id: 1, name: 'course1'},
-    {id: 2, name: 'course2'},
-    {id: 3, name: 'course3'},
-];
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
-// app.get('/api/courses', (req, res) => {
-//     res.send([1, 2, 3]);
-// });
-
-// app.get('/api/courses/:year/:month', (req, res) => {
-//     res.send(req.params);
-// });
-=======
 // console.log(`NODE_ENV ${process.env.NODE_ENV}`);
 // console.log(`app ${app.get('env')}`);
->>>>>>> e7939234a2a0433c3a6d89e7674322d7d7a86cbd
 
 app.set('view engine', 'pug');
 app.set('views', './views');
