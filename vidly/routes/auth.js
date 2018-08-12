b496cc0aa569efb7f4d7de4bd8bc420fb6bb55fb
Yuqing Lu <luyuqing0708@gmail.com>
2018-08-12 12:56:07 +0200

    Add authentication

---
+++ vidly/routes/auth.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcrypt');
+const Joi = require('joi');
+const { User } = require('../models/users');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const { error } = validateReq(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send('Invalid email or password.');
+
+    const validPassword = bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send('Invalid email or password.');
+
+    res.send(true);
+  });
+
+function validateReq(req) {
+    const schema = {
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    };
+
+    return Joi.validate(req, schema);
+}
+
+module.exports = router;
